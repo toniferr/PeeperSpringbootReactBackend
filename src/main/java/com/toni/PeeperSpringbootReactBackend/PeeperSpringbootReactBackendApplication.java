79794e88372c9130618ef9fdcb2b60d3ca850edf
Toni <ferreirocouto@gmail.com>
2021-11-08 12:59:35 +0100

    first commit

---
+++ src/main/java/com/toni/PeeperSpringbootReactBackend/PeeperSpringbootReactBackendApplication.java
@@ -0,0 +1,13 @@
+package com.toni.PeeperSpringbootReactBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeeperSpringbootReactBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeeperSpringbootReactBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toni/PeeperSpringbootReactBackend/PeeperSpringbootReactBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toni.PeeperSpringbootReactBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeeperSpringbootReactBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
